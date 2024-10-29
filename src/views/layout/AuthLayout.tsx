0bad3a252edb44d0e68ad54e1ca1fa8a3f0d2532
anand443 <anand@cytomate.net>
2024-10-29 23:39:05 +0500

    login route updated

--- src/views/layout/AuthLayout.tsx
+++ src/views/layout/AuthLayout.tsx
@@ -41,7 +41,7 @@ const AuthLayout: React.FC = () => {
     (async () => {
       if (isAuthenticated && role) {
         if (role === UserRoles.Client) {
-          navigate('/user/essential-eight');
+          navigate('/user/');
         } else if (role === UserRoles.Mssp) {
           // If not redirect to cached state
           navigate('/mssp/');
